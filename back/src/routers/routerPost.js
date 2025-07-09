cd15170264cdaceeb9a072689fca3b23a6b77843
fabian <lugia759@gmail.com>
2025-07-08 20:30:50 -0500

    update Post

--- back/src/routers/routerPost.js
+++ back/src/routers/routerPost.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import controllerPosts from '../controllers/controllerPosts.js';
+import { uploadSingleImage } from '../middlewares/upload.js';
 
 const routerPosts = Router();
 
-routerPosts.post('/', controllerPosts.crearPost);        // Crear post (con imagen)
+routerPosts.post('/', uploadSingleImage, controllerPosts.crearPost);       // Crear post (con imagen)
 routerPosts.get('/', controllerPosts.listarPosts);       // Listar todos los posts
 routerPosts.get('/:id', controllerPosts.verPost);        // Ver un post por ID
 routerPosts.delete('/:id', controllerPosts.eliminarPost); // Eliminar un post
