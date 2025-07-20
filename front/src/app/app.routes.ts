9018529882ee556450b42deb0de04e6a89e80abe
Joan Cruz <joancruzcortes@gmail.com>
2025-07-20 17:27:12 -0500

    profile and register updated

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { Home } from './components/home/home';
 import { PageNotFound } from './components/page-not-found/page-not-found';
 import { Posts } from './components/posts/posts';
 import { Register } from './components/register/register';
-// import { Profile } from './components/profile/profile';
+import { Profile } from './components/profile/profile';
 
 export const routes: Routes = [
   { path: 'home', title: 'Home', component: Home },
   { path: 'posts', title: 'Explorar', component: Posts },
   { path: 'register', title: 'Registro', component: Register },
-//   { path: 'profile', title: 'Perfil', component: Profile },
+  { path: 'profile', title: 'Perfil', component: Profile },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', title: '404 | Page Not Found', component: PageNotFound }
 ];
