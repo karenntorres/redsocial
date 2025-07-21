738bfabbdc8ebdb2bcecd7a878de2fff551d5455
KarennTorres <kar33en@gmail.com>
2025-07-20 21:24:33 -0400

    main-glim

---
+++ front/src/app/components/main-glim/main-glim.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-glim',
+  imports: [],
+  templateUrl: './main-glim.html',
+  styleUrl: './main-glim.css'
+})
+export class MainGlim {
+
+}
