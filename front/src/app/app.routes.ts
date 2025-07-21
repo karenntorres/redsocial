0e6f47e7f9068b8e8c2d59bc797dd66b22304ca0
Joan Cruz <joancruzcortes@gmail.com>
2025-07-20 21:24:46 -0500

    interfaces and services created

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,15 +1,20 @@
 import { Routes } from '@angular/router';
-import { Home } from './components/home/home';
-import { PageNotFound } from './components/page-not-found/page-not-found';
+import { MainGlim } from './components/main-glim/main-glim';
 import { Posts } from './components/posts/posts';
 import { Register } from './components/register/register';
+import { Login } from './components/login/login';
+import { ForgotPassword } from './components/forgot-password/forgot-password';
 import { Profile } from './components/profile/profile';
+import { PageNotFound } from './components/page-not-found/page-not-found';
 
 export const routes: Routes = [
-  { path: 'home', title: 'Home', component: Home },
-  { path: 'posts', title: 'Explorar', component: Posts },
-  { path: 'register', title: 'Registro', component: Register },
-  { path: 'profile', title: 'Perfil', component: Profile },
+  { path: 'main', title: 'Main', component: MainGlim},
+  { path: 'posts', title: 'Explorar', component: Posts }, // Same FEED 
+  { path: 'register', title: 'Register', component: Register},
+  { path: 'login', title: 'Login', component: Login},
+  { path: 'forgot-password', title: 'Forgot-password', component: ForgotPassword},
+  { path: 'profile', title: 'Profile', component: Profile },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', title: '404 | Page Not Found', component: PageNotFound }
 ];
+
