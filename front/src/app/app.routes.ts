6475bae199af3d4a294d07fcdd7d4f8f76a6a9ae
Joan Cruz <joancruzcortes@gmail.com>
2025-07-21 19:56:18 -0500

    register component, service and interface running

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainGlim } from './components/main-glim/main-glim';
 import { Posts } from './components/posts/posts';
-import { Register } from './components/register/register';
+import { RegisterComponent } from './components/register/register';
 import { Login } from './components/login/login';
 import { ForgotPassword } from './components/forgot-password/forgot-password';
 import { Profile } from './components/profile/profile';
@@ -10,11 +10,11 @@ import { PageNotFound } from './components/page-not-found/page-not-found';
 export const routes: Routes = [
   { path: 'main', title: 'Main', component: MainGlim},
   { path: 'posts', title: 'Explorar', component: Posts }, // Same FEED 
-  { path: 'register', title: 'Register', component: Register},
+  { path: 'register', title: 'Register', component: RegisterComponent},
   { path: 'login', title: 'Login', component: Login},
   { path: 'forgot-password', title: 'Forgot-password', component: ForgotPassword},
   { path: 'profile', title: 'Profile', component: Profile },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
   { path: '**', title: '404 | Page Not Found', component: PageNotFound }
 ];
 
---
+++ front/src/app/interfaces/registerUsers.ts
@@ -0,0 +1,9 @@
+export interface Users {
+    name: string; 
+    email: string;
+    password: string;
+    username: string;
+    pfPicture: string;
+}
+
+
