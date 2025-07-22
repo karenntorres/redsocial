3921943c96e2416296b0a35555e631359242c49b
KarennTorres <kar33en@gmail.com>
2025-07-21 21:15:20 -0400

    changes made

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,23 +1,19 @@
 import { Routes } from '@angular/router';
 import { MainGlim } from './components/main-glim/main-glim';
 import { Posts } from './components/posts/posts';
-import { Register } from './components/register/register';
+import { RegisterComponent } from './components/register/register';
 import { Login } from './components/login/login';
 import { ForgotPassword } from './components/forgot-password/forgot-password';
 import { Profile } from './components/profile/profile';
 import { PageNotFound } from './components/page-not-found/page-not-found';
-import { guestGuard } from './guards/guest-guard';
-import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-  { path: 'main', title: 'Main', component: MainGlim, canActivate: [authGuard] },
-  { path: 'posts', title: 'Explorar', component: Posts, canActivate: [authGuard] },
-  { path: 'profile', title: 'Profile', component: Profile, canActivate: [authGuard] },
-
-  { path: 'login', title: 'Login', component: Login, canActivate: [guestGuard] },
-  { path: 'register', title: 'Register', component: Register, canActivate: [guestGuard] },
-  { path: 'forgot-password', title: 'Forgot-password', component: ForgotPassword },
-
+  { path: 'main', title: 'Main', component: MainGlim},
+  { path: 'posts', title: 'Explorar', component: Posts }, // Same FEED 
+  { path: 'register', title: 'Register', component: RegisterComponent},
+  { path: 'login', title: 'Login', component: Login},
+  { path: 'forgot-password', title: 'Forgot-password', component: ForgotPassword},
+  { path: 'profile', title: 'Profile', component: Profile },
   { path: '', redirectTo: 'main', pathMatch: 'full' },
   { path: '**', title: '404 | Page Not Found', component: PageNotFound }
 ];
