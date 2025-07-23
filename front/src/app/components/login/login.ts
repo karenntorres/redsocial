92cf90ef9e047a49052b15e9b2495fbcdc4b59b2
KarennTorres <kar33en@gmail.com>
2025-07-23 19:43:25 -0400

    changes uploaded

--- front/src/app/components/login/login.ts
+++ front/src/app/components/login/login.ts
@@ -1,1 +1,59 @@
- this.isLoggedIn = !!localStorage.getItem('userToken'); // Ajusta el nombre si usaste otro
+import { Component, inject } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { LoginService } from '../../services/login-service';
+import { Credentials } from '../../interfaces/credentials';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.html',
+  styleUrls: ['./login.css'],
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink, RouterModule],
+})
+export class Login {
+  router = inject(Router);
+  loginService = inject(LoginService);
+
+  credentialForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  handleSubmit() {
+    if (this.credentialForm.valid) {
+      const email = this.credentialForm.value.email!;
+      const password = this.credentialForm.value.password!;
+
+      const credentials: Credentials = { email, password };
+
+      this.loginService.login(credentials).subscribe((response: any) => {
+        if (response.result === 'fine') {
+          localStorage.setItem('token', response.data);
+
+          const decoded: any = this.loginService.decodeToken(response.data);
+          console.log('Decoded token:', decoded);
+
+          if (decoded.rol === 'admin') {
+            this.router.navigateByUrl('/posts');
+          } else {
+            this.router.navigateByUrl('/main-glim');
+          }
+        } else {
+          alert('Login failed: ' + response.message);
+        }
+      });
+    } else {
+      alert('Please fill in both fields.');
+    }
+  }
+}
+
+
+// comentario prueba 
