1e2c3ff85a18fadf65fed3edfdeaab6472c6c55b
fabian <lugia759@gmail.com>
2025-07-22 19:13:25 -0500

    update posts

--- front/src/app/interfaces/posts.ts
+++ front/src/app/interfaces/posts.ts
@@ -1,3 +1,4 @@
 export interface Posts {
-    
+    contenido: string; 
+    imagen?: string;  
 }
--- front/src/app/services/posts-service.ts
+++ front/src/app/services/posts-service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Posts } from '../interfaces/posts'; 
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PostsService {
-  
+export class PostService {
+
+  private API_URL = 'http://localhost:3001/posts';
+
+  constructor(private http: HttpClient) {}
+
+  // Crear nuevo post
+  createPost(post: Posts): Observable<Posts> {
+    return this.http.post<Posts>(this.API_URL, post);
+  }
+
+  // Obtener todos los posts
+  getPosts(): Observable<Posts[]> {
+    return this.http.get<Posts[]>(this.API_URL);
+  }
+
+  // Obtener un post por ID
+  getPostById(id: string): Observable<Posts> {
+    return this.http.get<Posts>(`${this.API_URL}/${id}`);
+  }
+
+  // Actualizar un post
+  updatePost(id: string, post: Posts): Observable<Posts> {
+    return this.http.put<Posts>(`${this.API_URL}/${id}`, post);
+  }
+
+  // Eliminar un post
+  deletePost(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.API_URL}/${id}`);
+  }
 }
